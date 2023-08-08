5a0c6b76772dd2e3013c7853a516b8d6616faed6
Your Name <benja902674@gmail.com>
2023-08-08 14:37:29 -0500

    nuevo

--- js/javamenu.js
+++ js/javamenu.js
@@ -5,7 +5,7 @@ let scrollPrecentage = () =>{
   let pos = document.documentElement.scrollTop;
   let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   let scrollValue = Math.round(pos * 100 / calcHeight);
-  scrollProgress.style.background = `conic-gradient(#e70634 ${scrollValue}%, #2b2f38 ${scrollValue}%)`;
+  scrollProgress.style.background = `conic-gradient(#0AA2C0 ${scrollValue}%, #2b2f38 ${scrollValue}%)`;
 }
 window.onscroll = scrollPrecentage;
 window.onload = scrollPrecentage;
